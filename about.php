928ff8505bcd264d9e319f38caabb221da071f42
Archana Kushwaha <webwitharchana22@gmail.com>
2022-06-19 17:43:41 +0530

    archana kushwaha is a creator
    web with archana  this is my website

---
+++ about.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Evolution-About Us</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,300&family=Redressed&family=Roboto+Mono&display=swap" rel="stylesheet">
+
+<?php include'navbar.html'?>
+<style type="text/css">
+*{margin: 0;padding: 0; box-sizing: border-box;}
+
+.container-fluid h1{
+	width: 500px;
+    margin-left: 900px;
+	text-align: center;
+	font-family: 'Roboto Mono', monospace;
+	background-color: #00b894;
+	color: white;
+   text-shadow: -4px -4px 4px rgba(0, 0, 0, 1);
+   text-transform: uppercase;
+   font-weight: bold;
+   margin-top: 30px; 
+   margin-bottom: 30px;
+}
+.about{
+	height: 50vh;
+	width: 100%;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	box-sizing: border-box;
+}
+.para{
+	width: 50%;
+	padding: 0 5%;
+	box-sizing: border-box;
+	text-align: justify;
+	line-height: 1.7;
+	margin-bottom: 200px;
+}
+.para h2{
+    text-align: center;
+	background:rgba(0,0,0,0.3);
+	font-family: 'Josefin Sans', sans-serif;
+	background-color: #00b894;
+	color: white;
+   text-shadow: -4px -4px 4px rgba(0, 0, 0, 2);
+   text-transform: uppercase;
+   margin-top: 0px;
+   margin-bottom: 2px;
+}
+.para p{
+	padding-left: 0;
+	margin-left: 0;
+	margin-bottom: 2px;
+    text-align: justify;
+	font-family: 'Redressed', cursive;
+}
+.image{
+	margin-top: 0px;
+	margin-bottom:0px;
+}
+.image a{
+	text-decoration: none;
+	width: 50%;
+	padding:0;
+	text-align: center;
+}
+.image img{
+	margin-top: 100px;
+	margin-bottom: 200px;
+}
+@media only screen and (max-device-width: 768px) {
+	.container-fluid h1{
+    margin-left: 89px;
+    margin-bottom: 0px;
+}
+.about{
+	display: block;
+}
+.para{
+	width: 100%;
+	box-sizing: border-box;
+	text-align: justify;
+	line-height: 1.7;
+	margin-bottom: 5px;
+}
+.para h2{
+	 margin-top: 0px;
+   margin-bottom: 2px;
+	width: 600px;
+ 
+}
+.para p{
+	margin-right: 0;
+	margin-left: 7px;
+	margin-bottom: 0px;
+	line-height: 1.5;
+	word-spacing: 0.5;
+	letter-spacing: 1px;
+	font-size: 1.2rem;
+}
+.image{
+	display: block;
+	margin-bottom:0px;
+}
+.image a{
+	margin-top: 10px;
+	margin-bottom: 0px;
+}
+.image img{
+	width: 100%;
+	height: auto;
+	margin-top: 6px;
+	margin-bottom: 0px;
+}
+.footer{
+	margin-top: 750px;
+
+}
+.footer_style{
+box-sizing: border-box;
+padding-bottom: 10px;
+}
+}
+</style>
+
+
+</head>
+<body>
+<section class="my-3">
+<div class="py-3">
+<h2 class="text-center">About Us</h2>   
+</div>
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-6 col-md-6 col-12">
+<img src="img/office.jpg" class="img-fluid" aboutimg>
+</div>
+<div class="col-lg-6 col-md-6 col-12">
+<h2 class="display-4">Web With Archana</h2>
+<p class="py-3"> Web With Archana supplies ,inc.,or simply,designer and secure websites is an Indian website with corporate offices in Jabalpur.Web With Archana  website work fast and this website  services are very good, as well as create live websites. services includes explained whole information on any type of website.This website is developed by Archana Kushwaha and her hardwork. </p>
+<a href="#" class="btn btn-success">About Us</a>
+</div>   
+</div> 
+</div>
+</section>    
+ 
+<div class="footer">
+<?php include'footer.html'?>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+<h1>These are a very important in this over a world are the are one of the most common input field one of the best are that one of the are a drama tat is a queen of the not for that over all one of the best for that no all for that one are good that one of most common languange.</h1>
+</body>
+</html>
---
+++ web_with_archana.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<meta charset="utf-8">
+<title>Web With Archana</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Caveat:wght@500&family=Fredoka+One&family=Ravi+Prakash&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@1,100&family=RocknRoll+One&family=Ubuntu:ital,wght@1,300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@500&display=swap" rel="stylesheet">
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<style type="text/css">
+	nav ul li a{
+		text-decoration: none;
+	}
+</style>
+
+</head>
+<body>
+<header>
+<nav>
+<input type="checkbox" id="check">
+<label for="check" class="checkbtn">
+<i class="fas fa-bars"></i>
+</label>
+<label class="logo animated infinite heartBeat">Web With Archana</label>
+<ul style="text-decoration: none;">
+
+<li><a href="home.html" style="text-decoration:none;" class="acive">Home</a></li>
+<li><a href="#" style="text-decoration:none;">AboutUs</a></li>
+<li><a href="#" style="text-decoration:none;">ContactUs</a></li>
+<li><a href="#" style="text-decoration:none;">Login</a></li>
+
+</li>
+</ul>
+
+</nav>
+<main>
+<section>
+<h3 style="padding-top: auto;">Welcome To Web With Archana</h3>    
+<h1>would you like to Unearth your Website In  <span class="change_content"></span></h1>
+<p>we  can made  stactics websites for your</p>
+</section>    
+</main>
+</header>
+
+<section class="my-3">
+<div class="py-3">
+<h2 class="text-center" >Services</h2>   
+</div>
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-4 col-md-4 col-12" style="padding-left: 30px;"> 
+<div class="card">
+<img class="card-img-top" src="img/creativity-819371_1920.jpg" alt="Card image" width="600" height="400">
+<div class="card-body">
+<h4 class="card-title" style="color: #00b894; text-shadow: 1px 3px 0 #969696, 1px 4px 5px #aba8a8, -9px -19px 5px rgba(206,89,55,0); ">Different Ideas!</h4>
+<p class="card-text">Make different website.</p>
+<a href="#" class="btn btn-primary">Check More</a>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-12" style="padding-right: 30px;"> 
+<div class="card">
+<img class="card-img-top" src="img/pexels-pixabay-159299.jpg" alt="Card image" width="600" height="400">
+<div class="card-body">
+<h4 class="card-title" style="color: #00b894; text-shadow: 1px 3px 0 #969696, 1px 4px 5px #aba8a8, -9px -19px 5px rgba(206,89,55,0);">Social Services</h4>
+<p class="card-text">Explain Some New Features.</p>
+<a href="#" class="btn btn-primary">Check More</a>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-4 col-12"style="padding-right: 30px;"> 
+<div class="card">
+<img class="card-img-top" src="img/software-657188_1920.jpg" alt="Card image" width="600" height="400">
+<div class="card-body">
+<h4 class="card-title" style="color: #00b894; text-shadow: 1px 3px 0 #969696, 1px 4px 5px #aba8a8, -9px -19px 5px rgba(206,89,55,0);">Work Fast</h4>
+<p class="card-text">Website Make Faster.</p>
+<a href="#" class="btn btn-primary">Check More</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<div class="learning_ideas">
+<section class="study_material"> 
+<h1>Feel exhilrate with start <br>
+ something new <span class="study"></span></h1>
+<br><br><br><br>
+<a class="btn" href="#">learn more</a>
+</section>
+
+</div>
+
+<section class="my-3">
+<div class="py-3">
+<h2 class="text-center">About Us</h2>   
+</div>
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-6 col-md-6 col-12">
+<img src="img/office.jpg" class="img-fluid" aboutimg>
+</div>
+<div class="col-lg-6 col-md-6 col-12">
+<h2 class="display-4">Web With Archana</h2>
+<p class="py-3"> Web With Archana supplies ,inc.,or simply,designer and secure websites is an Indian website with corporate offices in Jabalpur.Web With Archana  website work fast and this website  services are very good, as well as create live websites. services includes explained whole information on any type of website.This website is developed by Archana Kushwaha and her hardwork. </p>
+<a href="#" class="btn btn-success">About Us</a>
+</div>   
+</div> 
+</div>
+</section>    
+ 
+<div class="footer">
+<?php include'footer.html'?>
+</div>
+
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
